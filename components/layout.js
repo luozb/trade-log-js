02d53a0e5df7f65806a5c234ab74fabd14f04310
txl <txl@bit-z.pro>
2022-03-18 16:51:14 +0800

    add create

--- components/layout.js
+++ components/layout.js
@@ -33,11 +33,11 @@ export default function Layout({ children, home }) {
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={108}
+              width={108}
               alt={name}
             />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <h2 className={utilStyles.colorInherit}>{name}</h2>
           </>
         ) : (
           <>
---
+++ pages/create.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Create() {
+  return (
+    <Layout>
+      <div>create</div>
+    </Layout>
+  );
+}
